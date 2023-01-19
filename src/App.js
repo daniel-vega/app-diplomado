f96482ff4609cd217d2a9ce3524f278d17c1c126
Daniel Vega <daniel.vega.a@gmail.com>
2023-01-18 21:44:23 -0300

    cambios menores

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with diplomado..
+          Learn React with diplomado..lskdhflksdhflksdhfkjsdhfkjsdhfkjsdhfkjsdhk
         </a>
       </header>
     </div>
